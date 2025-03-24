85eac55c77222e749f15d6c61fa90b2a89159bd8
zakariae el hassad <zakariaelhassad031@gmail.com>
2025-03-24 12:20:12 +0000

    ajouter une migration de Seance

---
+++ database/migrations/2025_03_24_115723_create_seance_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('seance', function (Blueprint $table) {
+            $table->id(); 
+            $table->unsignedBigInteger('salle_id');
+            $table->unsignedBigInteger('film_id');
+            $table->dateTime('date_heure'); 
+            $table->enum('type', ['Normale', 'VIP']);
+            $table->timestamps();
+
+            $table->foreign('salle_id')->references('id')->on('salle')->onDelete('cascade');
+            $table->foreign('film_id')->references('id')->on('film')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('seance');
+    }
+};
