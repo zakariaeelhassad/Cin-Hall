3213cbe5e9c850ac810cf475469cb41869b5ec5d
zakariae el hassad <zakariaelhassad031@gmail.com>
2025-03-24 12:19:04 +0000

    ajouter un dossier Service qui contient des fichier FilmService , SalleService , SeanceService , SiegeService et UserService .

---
+++ app/Services/FilmService.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Services;
+
+use App\Repositories\FilmRepository;
+
+class FilmService
+{
+    private FilmRepository $repository;
+
+    public function __construct(FilmRepository $repository) {
+        $this->repository = $repository;
+    }
+
+    public function create(array $data)
+    {
+        return $this->repository->create($data);
+    }
+
+    public function update(array $data, int $id)
+    {
+        return $this->repository->update($data, $id);
+    }
+
+    public function delete(int $id)
+    {
+        $this->repository->delete($id);
+    }
+
+    public function all()
+    {
+        return $this->repository->all();
+    }
+
+    public function find(int $id)
+    {
+        return $this->repository->find($id);
+    }
+}
---
+++ app/Services/SalleService.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Services;
+
+use App\Repositories\SalleRepository;
+
+class SalleService
+{
+    private SalleRepository $repository;
+
+    public function __construct(SalleRepository $repository) {
+        $this->repository = $repository;
+    }
+
+    public function create(array $data)
+    {
+        return $this->repository->create($data);
+    }
+
+    public function update(array $data, int $id)
+    {
+        return $this->repository->update($data, $id);
+    }
+
+    public function delete(int $id)
+    {
+        $this->repository->delete($id);
+    }
+
+    public function all()
+    {
+        return $this->repository->all();
+    }
+
+    public function find(int $id)
+    {
+        return $this->repository->find($id);
+    }
+}
