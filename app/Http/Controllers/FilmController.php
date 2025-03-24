3e2b3bd7dd270c98cc124c73b299f39064df0d78
zakariae el hassad <zakariaelhassad031@gmail.com>
2025-03-24 12:21:41 +0000

    ajouter une controller de Film

--- app/Http/Controllers/FilmController.php
+++ app/Http/Controllers/FilmController.php
@@ -2,34 +2,34 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Controllers\Controller;
 use App\Models\Film;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
+use App\Services\FilmService;
+use Symfony\Component\HttpFoundation\Response;
 
 class FilmController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
+    protected FilmService $filmService;
+
+    public function __construct(FilmService $filmService)
     {
-        $films = Film::with('user')->get(); 
-        return response()->json($films);
+        $this->filmService = $filmService;
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
+    public function index()
+    {
+        $films = $this->filmService->all();
+
+        return response()->json([
+            'status' => 'success',
+            'data' => $films
+        ], 200);
+    }
+
     public function store(Request $request)
     {
-
-        if (Auth::user()->role !== 'admin') {
-            return response()->json([
-                'message' => 'Vous n\'êtes pas autorisé à ajouter un film.'
-            ], 403); 
-        }
-
-        $request->validate([
+        $data = $request->validate([
             'titre' => 'required|string',
             'description' => 'nullable|string',
             'image' => 'nullable|string',
@@ -40,45 +40,82 @@ class FilmController extends Controller
             'genre' => 'required|string',
         ]);
 
-        $film = Film::create([
-            'admin_id' => Auth::id(), 
-            'titre' => $request->titre,
-            'description' => $request->description,
-            'image' => $request->image,
-            'durée' => $request->durée,
-            'langue' => $request->langue,
-            'age_min' => $request->age_min,
-            'bande_annonce' => $request->bande_annonce,
-            'genre' => $request->genre,
-        ]);
+        $data['admin_id'] = auth('api')->id();
+        $film = $this->filmService->create($data);
+
+        if (!$film instanceof Film) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Failed to create film'
+            ],500);
+        }
 
         return response()->json([
-            'message' => 'Film ajouté avec succès',
-            'film' => $film
+            'status' => 'success',
+            'data' => $film
         ], 201);
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
+    public function show(int $id)
     {
-        //
+        $film = $this->filmService->find($id);
+
+        if (!$film instanceof Film) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Film not found'
+            ], 404);
+        }
+
+        return response()->json([
+            'status' => 'success',
+            'data' => $film
+        ], 200);
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
+    public function update(Request $request, int $id)
     {
-        //
+        $data = $request->validate([
+            'titre' => 'required|string',
+            'description' => 'nullable|string',
+            'image' => 'nullable|string',
+            'durée' => 'required|integer',
+            'langue' => 'required|string',
+            'age_min' => 'required|integer',
+            'bande_annonce' => 'nullable|string',
+            'genre' => 'required|string',
+        ]);
+
+        $result = $this->filmService->update($data, $id);
+
+        if ($result <= 0) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Failed to update film'
+            ], 500);
+        }
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'film updated successfully'
+        ],200);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
+    public function destroy(int $id)
     {
-        //
+        $result = $this->filmService->delete($id);
+
+        if (!is_bool($result)) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Failed to delete film'
+            ], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'film deleted successfully'
+        ], 200);
     }
 }
+
